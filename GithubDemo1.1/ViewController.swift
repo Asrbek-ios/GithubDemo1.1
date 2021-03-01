73e2c514ddc03787b63b0e44ca0d2c6fcc9ac3c2
AsrbekMahmudov <maxmudovasrbek777@gmail.com>
2021-03-01 21:05:20 +0500

    Initial Commit

---
+++ GithubDemo1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubDemo1.1
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
